4911ccb9c50e53a68f05161c975aaa4e8c5f1ef8
Chukwudi Udechukwu <162521217+cudechukwu@users.noreply.github.com>
2025-03-06 16:03:36 -0500

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; // XAMPP runs MySQL locally
 $username = "root"; // Default MySQL username
 $password = ""; // Default MySQL password in XAMPP (leave empty)
-$database = "app_db"; // Your database name
+$database = "app-db"; // Your database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
